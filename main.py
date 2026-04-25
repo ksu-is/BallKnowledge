7614e6da314432a84c411e5cab62becc8ae99b33
gzavala1 <147107087+gzavala1@users.noreply.github.com>
2026-04-25 11:28:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Quiz Application
+#TestRun
 quiz = [{"questions":"What teams played in the 2019 UEFA UCL final?","options":"A) PSG vs. FCB\nB) RMA vs. FCB\nC) LIV vs. TOT\nD) SEV vs. MAN","answer":"C"},
         {"questions":"What city hosted the 2019 UEFA UCL final?","options":"A) Kennesaw\nB) Lisbon\nC) Amsterdam\nD) Madrid","answer":"D"},
         {"questions":"What was the final score of the 2019 UEFA UCL final?","options":"A) LIV 2 - TOT 0\nB) LIV 5 - TOT 10\nC) LIV 1 - TOT 2\nD) LIV 2 - TOT 3","answer":"A"}]
@@ -14,4 +14,4 @@ for q in quiz:
         print("Incorrect! The correct answer is:", q["answer"])
 print("\n Your final score is:",score)
 print("Thank you for playing the quiz!")
-# End of Quiz Application
+# End of Test Run
